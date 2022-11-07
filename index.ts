6d8a53aa46fab441e22d855a5d2e436c2c84b755
Charles Park <charlieboyee@gmail.com>
2022-11-07 11:30:29 -0800

    Finished. Consider creating user input so that when the Downloads directory changes, user can specify where to look for the directory.

--- index.ts
+++ index.ts
@@ -1,44 +1,50 @@
 const fs = require('fs/promises');
 const path = require('path');
+const mv = require('mv')
 
-const main = async() => {
-    const downloadsDirectory: string  = path.join(process.cwd(),'../../Downloads');
-    
-    try{
-        const files : string[] = await fs.readdir(downloadsDirectory);
+const main = async () => {
+    console.time('main');
+    const downloadsDirectory: string = path.join(process.cwd(), '../../Downloads');
+
+    try {
+        const files: string[] = await fs.readdir(downloadsDirectory);
         const folders: string[] = [];
+        const map= new Map();
 
-        const map = new Map();
-        let file: string 
-        for (file of files){
-            
+        for (const file of files) {
+
             const extension: string = path.extname(file);
             const fileName: string = path.basename(file);
             if (extension === '') {
                 folders.push(fileName)
                 continue
             }
-            if (!map.has(extension)){
+            if (!map.has(extension)) {
                 map.set(extension, [fileName])
             } else {
                 map.get(extension).push(fileName)
             }
         };
-        let ext: string
-        for (ext of map.keys()){
-            
-            if (!folders.includes(ext)){
-                console.log(ext)
-                const folder = path.join(downloadsDirectory,ext)
-                // fs.mkdir(folder);    
-            };
+
+        for (const [ext, values] of map) {
+            const newFolder: string= path.join(downloadsDirectory, ext)
+            if (!folders.includes(ext)) {
+                folders.push(ext);
+                fs.mkdir(newFolder);
+            }
+            for (let i = 0; i < values.length; i++) {
+                const start: string = path.join(downloadsDirectory, values[i])
+                const destination: string =  path.join(newFolder, values[i])
+                await mv(start, destination, { mkdirp: true })
+            }
         };
-        
-    }catch(err){
+
+    } catch (err) {
         console.log(err)
+    } finally{
+        console.timeEnd('main');
     }
 };
 
-console.time('main');
+
 main();
-console.timeEnd('main');
